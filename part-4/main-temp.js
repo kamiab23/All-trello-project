32db02b2e58b8e037159981644d34b2bab9b7faa
kamiab <kamiabparhizian23@gmail.com>
2024-03-12 16:10:10 +0330

    v.2.1

--- part-4/main-temp.js
+++ part-4/main-temp.js
@@ -3,11 +3,11 @@ let array2 = [0, 0, 1, 5, 5, 5, 6, 8, 10];
 
 //your code
 Array.prototype.push.apply(array1, array2);
-console.log(array1);
+console.log("array1 and array2 merged: ", array1);
 // let arr = lastArr.split(" ");
 var uniqueArr = array1.filter((value, index, self) => {
   return self.indexOf(value) === index;
 });
 
 //output
-console.log(uniqueArr);
+console.log("array without duplicates: ", uniqueArr);
