4e4a9f945198a19cc95f5ad3229dbb2fee7b7943
kamiab <kamiabparhizian23@gmail.com>
2024-03-13 14:37:13 +0330

    nodejs test module v2.2

--- part-4/main-temp.js
+++ part-4/main-temp.js
@@ -1,13 +1,15 @@
 let array1 = [1, 2, 3, 3, 4, 6, 6, 7, 8];
 let array2 = [0, 0, 1, 5, 5, 5, 6, 8, 10];
+let arr = [];
 
 //your code
-Array.prototype.push.apply(array1, array2);
-console.log("array1 and array2 merged: ", array1);
-// let arr = lastArr.split(" ");
-var uniqueArr = array1.filter((value, index, self) => {
-  return self.indexOf(value) === index;
-});
+export default function module() {
+  arr = arr.push.apply(array1, array2);
+  console.log("array1 and array2 merged: ", array1);
+  var uniqueArr = array1.filter((value, index, self) => {
+    return self.indexOf(value) === index;
+  });
 
-//output
-console.log("array without duplicates: ", uniqueArr);
+  //output
+  console.log("array without duplicates: ", uniqueArr);
+}
