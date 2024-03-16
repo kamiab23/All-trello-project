32bae9b05beb0fa53f1b34945245dc020eead17d
kamiab <kamiabparhizian23@gmail.com>
2024-03-16 19:06:43 +0330

    final version

--- part-4/main-temp.js
+++ part-4/main-temp.js
@@ -1,15 +1,13 @@
-let array1 = [1, 2, 3, 3, 4, 6, 6, 7, 8];
-let array2 = [0, 0, 1, 5, 5, 5, 6, 8, 10];
-let arr = [];
-
 //your code
-export default function module() {
-  arr = arr.push.apply(array1, array2);
-  console.log("array1 and array2 merged: ", array1);
-  var uniqueArr = array1.filter((value, index, self) => {
+export function module(array1, array2) {
+  let arr = [...array1, ...array2];
+  console.log("array1 and array2 merged: ", arr);
+  return arr;
+}
+export function removeDuplicates(arr) {
+  var uniqueArr = arr.filter((value, index, self) => {
     return self.indexOf(value) === index;
   });
-
-  //output
   console.log("array without duplicates: ", uniqueArr);
+  return uniqueArr;
 }
--- part-4/module.mjs
+++ part-4/module.mjs
@@ -1,2 +1,7 @@
-import module from "./main-temp.js";
-module();
+let array1 = [1, 2, 3, 3, 4, 6, 6, 7, 8];
+let array2 = [0, 0, 1, 5, 5, 5, 6, 8, 10];
+// let array3 = [0, 4, 302, 3, 1, 6, 8];
+import {module} from "./main-temp.js";
+import {removeDuplicates} from "./main-temp.js";
+// module(array1, array2);
+removeDuplicates(module(array1, array2));
